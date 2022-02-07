78e9e8f749cbc472f8d7aed1715bc0708132c7d9
Paolo <paolobadi76@gmail.com>
2022-02-07 21:32:56 +0100

    Parameters file

---
+++ include/parameters.h
@@ -0,0 +1,17 @@
+#ifndef parameters_h
+#define parameters_h
+
+#define echoPin 9       // servo-1 motor shield
+#define trigPin 10      // servo-2 motor shield
+
+#define MIN_DISTANCE 5  // cm
+#define MOTOR_SPEED 170
+
+#define CHECK_INTERVAL 500
+#define TURN_INTERVAL 2000
+
+#define STATUS_FORWARD 'F'
+#define STATUS_STOP 'S'
+#define STATUS_TURNRIGHT 'R'
+
+#endif
